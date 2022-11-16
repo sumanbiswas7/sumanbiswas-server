c733fa212b2d85ddd0ca02f82001d98e2a6d12ed
suman <ronaldosuman97@gmail.com>
2022-11-16 06:23:53 +0530

    add: views increment

--- dist/routes/viewsRoute.js
+++ dist/routes/viewsRoute.js
@@ -14,13 +14,21 @@ const database_1 = require("firebase/database");
 const firebase_1 = require("../database/firebase");
 const router = (0, express_1.Router)();
 router.post("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    // increments total views by 1
+    // @params: count: number(current views)
+    const prevCount = req.query.count;
+    const intCount = parseInt(prevCount);
+    if (!intCount)
+        return res.send(`Missing Query Params: @count`);
+    (0, database_1.set)((0, database_1.ref)(firebase_1.database, "/views"), { views: intCount + 1 });
+    (0, database_1.goOffline)(firebase_1.database);
+    return res.send(`views increment: successful`);
 }));
 router.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     // returns total views or 0 
-    const db = (0, database_1.getDatabase)(firebase_1.app);
-    const data = yield (0, database_1.get)((0, database_1.ref)(db, "/viewss"));
+    const data = yield (0, database_1.get)((0, database_1.ref)(firebase_1.database, "/views"));
     const response = data.val() || { views: 0 };
-    (0, database_1.goOffline)(db);
+    (0, database_1.goOffline)(firebase_1.database);
     return res.send(response);
 }));
 exports.default = router;
--- routes/viewsRoute.ts
+++ routes/viewsRoute.ts
@@ -1,18 +1,26 @@
 import { Router } from "express";
-import { getDatabase, ref, get, goOffline } from "firebase/database";
+import { set, ref, get, goOffline } from "firebase/database";
 import { app, database } from "../database/firebase";
 const router = Router();
 
 router.post("/", async (req, res) => {
+    // increments total views by 1
+    // @params: count: number(current views)
+    const prevCount = req.query.count as string;
+    const intCount = parseInt(prevCount)
+    if (!intCount) return res.send(`Missing Query Params: @count`)
 
+    set(ref(database, "/views"), { views: intCount + 1 })
+    goOffline(database)
+
+    return res.send(`views increment: successful`)
 })
 
 router.get("/", async (req, res) => {
     // returns total views or 0 
-    const db = getDatabase(app)
-    const data = await get(ref(db, "/viewss"))
+    const data = await get(ref(database, "/views"))
     const response = data.val() || { views: 0 }
-    goOffline(db)
+    goOffline(database)
     return res.send(response)
 
 })
