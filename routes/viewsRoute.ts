a600a2f7e8a60aea19d49c11e11832b4e0b37844
suman <ronaldosuman97@gmail.com>
2022-11-16 11:51:10 +0530

    refactor: viewsRoute

--- routes/viewsRoute.ts
+++ routes/viewsRoute.ts
@@ -1,25 +1,28 @@
 import { Router } from "express";
 import { set, ref, get, goOffline } from "firebase/database";
-import { app, database } from "../database/firebase";
+import { database } from "../database/firebase";
 const router = Router();
 
 router.post("/", async (req, res) => {
     // increments total views by 1
     // @params: count: number(current views)
-    const prevCount = req.query.count as string;
-    const intCount = parseInt(prevCount)
+
+    const prevCount = req.query.count;
+    const intCount = parseInt(<string>prevCount)
     if (!intCount) return res.send(`Missing Query Params: @count`)
 
     set(ref(database, "/views"), { views: intCount + 1 })
-    goOffline(database)
 
+    goOffline(database)
     return res.send(`views increment: successful`)
 })
 
 router.get("/", async (req, res) => {
     // returns total views or 0 
+
     const data = await get(ref(database, "/views"))
     const response = data.val() || { views: 0 }
+
     goOffline(database)
     return res.send(response)
 
