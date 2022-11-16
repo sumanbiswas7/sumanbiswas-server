479b738da518145f24af4c2fc078641c4cbc7647
suman <ronaldosuman97@gmail.com>
2022-11-16 12:46:02 +0530

    fix: heroku PORT

--- index.ts
+++ index.ts
@@ -3,11 +3,11 @@ import viewsRoute from "./routes/viewsRoute"
 import ipRoute from "./routes/ipRoute"
 const app = express();
 
+app.get("/", (req, res) => res.send("sumanbiswas-server"))
 app.use("/views", viewsRoute)
 app.use("/ip", ipRoute)
-app.get("/", (req, res) => res.send("sumanbiswas-server"))
 
 
-const PORT = process.env.port || 3999;
+const PORT = process.env.PORT || 3999;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
 
